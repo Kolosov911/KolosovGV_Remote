9302663824b8ddcfe3ff7350189e10c244a45eb8
Kolosov911 <adam_escobaro@mail.ru>
2026-03-09 18:58:05 +0300

    delete solve task2

--- KolosovGV/Program.cs
+++ KolosovGV/Program.cs
@@ -7,7 +7,6 @@
             Console.WriteLine("Hello, World!");
         }
         //Kolosov Gleb Am-1-25
-        //solve task2
         static public double Sqr(double x)
         { 
            return x;
